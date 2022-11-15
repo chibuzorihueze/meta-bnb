92985cd84f48a90b45a82ae1760174db1b989583
Chibuzor Ihueze <krisykord@gmail.com>
2022-11-15 05:17:31 +0100

    creating main footer component

---
+++ src/components/mainFooter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Mainfooter(){
+    return(
+        <div className="mainfooter-container">
+            <h2>MBToken</h2>
+            <h2>METAMASK</h2>
+            <h2>OpenSea</h2>
+        </div>
+    )
+}
+
+export default Mainfooter;
